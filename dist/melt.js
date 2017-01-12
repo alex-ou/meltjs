fd1ef9025fa1e903a7f04bea36de85efe0694777
Alex Ou <alex.ou@live.com>
2017-01-12 22:11:36 +1100

    finished the todo example

--- dist/melt.js
+++ dist/melt.js
@@ -896,7 +896,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	var unaryTag = (0, _index.makeMap)('area,base,basefont,br,col,frame,hr,img,input,link,meta,param,embed,command,keygen,source,track,wbr');
 
 	// Block Elements
-	var blockTag = (0, _index.makeMap)('a,address,article,applet,aside,audio,blockquote,button,canvas,center,dd,del,dir,div,dl,dt,fieldset,figcaption,figure,footer,form,frameset,h1,h2,h3,h4,h5,h6,header,hgroup,hr,iframe,ins,isindex,li,map,menu,noframes,noscript,object,ol,output,p,pre,section,script,table,tbody,td,tfoot,th,thead,tr,ul,video');
+	var blockTag = (0, _index.makeMap)('address,article,applet,aside,audio,blockquote,button,canvas,center,dd,del,dir,div,dl,dt,fieldset,figcaption,figure,footer,form,frameset,h1,h2,h3,h4,h5,h6,header,hgroup,hr,iframe,ins,isindex,li,map,menu,noframes,noscript,object,ol,output,p,pre,section,script,table,tbody,td,tfoot,th,thead,tr,ul,video');
 
 	// Inline Elements
 	var inlineTag = (0, _index.makeMap)('abbr,acronym,applet,b,basefont,bdo,big,br,button,cite,code,del,dfn,em,font,i,iframe,img,input,ins,kbd,label,map,object,q,s,samp,script,select,small,span,strike,strong,sub,sup,textarea,tt,u,var');
--- examples/todos/index.js
+++ examples/todos/index.js
@@ -1,16 +1,17 @@
 let nextId = 0
 function addTodo ({model}, text) {
-  return [
-    ...model,
+  const todos = [
+    ...model.todos,
     {
       id: nextId++,
       text: text,
       completed: false
     }]
+  return Object.assign({}, model, {todos})
 }
 
 function toggleTodo ({model}, id) {
-  return model.map(todo => {
+  const todos = model.todos.map(todo => {
     if (todo.id !== id) {
       return todo
     }
@@ -21,11 +22,16 @@ function toggleTodo ({model}, id) {
       completed: !todo.completed
     }
   })
+  return Object.assign({}, model, {todos})
+}
+
+function setVisibilityFilter ({model}, visibilityFilter) {
+  return Object.assign({}, model, {visibilityFilter})
 }
 
 Melt.component('todo', {
-  inputs: ['text', 'completed', 'onClick', 'todoId'],
-  template: '<li on-click="{onClick()}" class="{getClass()}">{text} {todoId}</li>',
+  inputs: ['text', 'completed', 'onClick'],
+  template: '<li on-click="{onClick()}" class="{getClass()}">{text}</li>',
   getClass: function () {
     return this.completed ? 'done' : ''
   }
@@ -35,16 +41,61 @@ Melt.component('todo-list', {
   inputs: ['todos', 'onTodoClick'],
   template:
     `<ul>
-      <todo each="item in todos" key="{item.id}" todo-id="{item.id}" completed="{item.completed}" text="{item.text}" on-click="{handleClick(item)}"></todo>
-    </ul>`,
-  handleClick: function (item) {
-    console.log('alex:', item)
-    this.onTodoClick(item.id)
-  }
+      <todo each="item in todos" key="{item.id}" completed="{item.completed}" text="{item.text}" on-click="{onTodoClick(item.id)}"></todo>
+    </ul>`
 })
 
 Melt.container('visible-todo-list', {
-  template: '<div></div>'
+  template: '<todo-list todos="{getVisibleTodos()}" on-todo-click="{toggleTodo}"></todo-list>',
+  getVisibleTodos: function () {
+    const {visibilityFilter, todos} = this.model
+    switch (visibilityFilter) {
+      case 'SHOW_ALL':
+        return todos
+      case 'SHOW_ACTIVE':
+        return todos.filter(todo => !todo.completed)
+      case 'SHOW_COMPLETED':
+        return todos.filter(todo => todo.completed)
+      default:
+        throw new Error('unsupported filter:' + visibilityFilter)
+    }
+  }
+})
+
+Melt.component('todo-link', {
+  inputs: ['active', 'onClick'],
+  template: `<span>
+      <span if="{active}">{children}</span>
+      <a if="{!active}" href="#" on-click="handleClick">{children}</a>'
+    </span>`,
+  handleClick: function (e) {
+    e.preventDefault()
+    this.onClick()
+  }
+})
+
+Melt.container('filter-link', {
+  inputs: ['filter'],
+  template: '<todo-link active="{filter === model.visibilityFilter}" on-click="setVisibilityFilter(filter)">{children}</todo-link>'
+})
+
+Melt.component('footer', {
+  template: `
+  <p>
+    Show:
+    {" "}
+    <filter-link filter="SHOW_ALL">
+      All
+    </filter-link>
+    {", "}
+    <filter-link filter="SHOW_ACTIVE">
+      Active
+    </filter-link>
+    {", "}
+    <filter-link filter="SHOW_COMPLETED">
+      Completed
+    </filter-link>
+  </p>`
 })
 
 Melt.container('add-todo', {
@@ -57,6 +108,7 @@ Melt.container('add-todo', {
   onInput: function (e) {
     this.inputText = e.target.value
   },
+
   onSubmit: function (e) {
     e.preventDefault()
     if (!this.inputText.trim()) {
@@ -70,16 +122,21 @@ Melt.container('app', {
   template:
     `<div>
       <add-todo></add-todo>
-      <todo-list todos="{model}" on-todo-click="{toggleTodo}"></todo-list>
+      <visible-todo-list></visible-todo-list>
+      <footer></footer>
     </div>`
 })
 
 Melt.app({
   el: '#app',
   template: '<app></app>',
-  model: [],
+  model: {
+    todos: [],
+    visibilityFilter: 'SHOW_ALL'
+  },
   update: {
     addTodo,
-    toggleTodo
+    toggleTodo,
+    setVisibilityFilter
   }
 })
